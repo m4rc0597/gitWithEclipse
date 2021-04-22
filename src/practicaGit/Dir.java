f18fb6c8e89879e1043704465a1e00a37e7dd223
Juankuus <juanc.mormar.1@educa.jcyl.es>
2021-04-22 18:51:24 +0200

    Jc:parte 1

--- src/practicaGit/Dir.java
+++ src/practicaGit/Dir.java
@@ -1,9 +1,38 @@
 package practicaGit;
-/**
- * 
- * @author tarde
- *
- */
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+
 public class Dir {
 
+	private String directorio;
+	private File carpeta;
+	private File[] archivos;
+	
+	public Dir() {
+		carpeta = new File(".");
+		directorio = ".";
+	}
+	public Dir(String directorio) {
+		this.directorio = directorio;
+		if (longitudDirectorio()) {
+			carpeta = new File(this.directorio.replace('/', File.separatorChar));
+			if (!carpeta.exists()) {
+				System.out.println("Ruta no valida, se asigna la del directorio actual.");
+				System.out.println("");
+				carpeta = new File(".");
+				this.directorio = ".";
+			}
+		}else {
+			carpeta = new File(".");
+			this.directorio = ".";
+		}
+	}
+	
+	public boolean longitudDirectorio() {
+		if (directorio.length()>0) return true;
+		else return false;
+	}
+	
+	
 }
